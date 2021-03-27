e1810cf4a38de52581e2cb55239b1aea33ad159b
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-03-27 16:56:55 +0300

    Добавлена директория server, которая будет содержать код FCGI-сервера

---
+++ server/main.cpp
@@ -0,0 +1,25 @@
+/// @file
+/// @brief
+/// @copyright Copyright (c) InfoTeCS. All Rights Reserved.
+
+#include <stdexcept>
+#include <iostream>
+
+#include <boost/core/ignore_unused.hpp>
+#include <boost/exception/diagnostic_information.hpp>
+
+
+int main( int argc, char** argv )
+{
+     boost::ignore_unused( argc, argv );
+     try
+     {
+
+     }
+     catch( const std::exception& e )
+     {
+          std::cerr << "exception: " << boost::diagnostic_information( e ) << '\n';
+          return 1;
+     }
+     return 0;
+}
