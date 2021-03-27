55296762c60713005bb433d5373abe7f36771403
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-03-27 17:16:01 +0300

    server: Написан код для приема и обрабобтки запроса

--- server/main.cpp
+++ server/main.cpp
@@ -9,6 +9,7 @@
 #include <boost/exception/diagnostic_information.hpp>
 
 #include <fcgiapp.h>
+#include <fcgio.h>
 
 
 #define THROW_EXCEPTION_IF( cond, msg, exc ) \
@@ -18,12 +19,35 @@
      THROW_EXCEPTION_IF( cond, msg, std::runtime_error )
 
 
+void handle( std::istream& request, std::ostream& response )
+{
+}
+
+
 int main( int argc, char** argv )
 {
      boost::ignore_unused( argc, argv );
      try
      {
           THROW_RUNTIME_ERROR_IF( FCGX_Init() != 0, "FCGX initialization error" );
+
+          FCGX_Request request;
+
+          static constexpr auto noSocket = 0;
+          static constexpr auto noFlags = 0;
+
+          THROW_RUNTIME_ERROR_IF(
+               FCGX_InitRequest( &request, noSocket, noFlags ) != 0
+               , "FCGX request initialization error"
+               );
+
+          while( FCGX_Accept_r( &request ) == 0 )
+          {
+               fcgi_istream is{ request.in };
+               fcgi_ostream os{ request.out };
+
+               handle( is, os );
+          }
      }
      catch( const std::exception& e )
      {
