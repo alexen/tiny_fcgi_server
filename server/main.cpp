270148651269d0c309f9ddd4454cc59661cadca6
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-03-27 17:04:01 +0300

    server: Добавлена инициализация библиотеки FCGI, прописана линковка

--- server/main.cpp
+++ server/main.cpp
@@ -8,13 +8,15 @@
 #include <boost/core/ignore_unused.hpp>
 #include <boost/exception/diagnostic_information.hpp>
 
+#include <fcgiapp.h>
+
 
 int main( int argc, char** argv )
 {
      boost::ignore_unused( argc, argv );
      try
      {
-
+          FCGX_Init();
      }
      catch( const std::exception& e )
      {
