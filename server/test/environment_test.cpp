7b5efc5f9c628a456893eadc521240b21cd74e2f
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-04-03 00:41:07 +0300

    server/test: Добавлена сборка unit-тестов для серверных компонент

--- server/test/environment_test.cpp
+++ server/test/environment_test.cpp
@@ -2,12 +2,29 @@
 /// @brief
 /// @copyright Copyright (c) InfoTeCS. All Rights Reserved.
 
-#include "environment.h"
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
 
-namespace alexen {
-namespace server {
-namespace fcgi {
+#include <boost/test/unit_test.hpp>
 
-} // namespace fcgi
-} // namespace server
-} // namespace alexen
+#include <server/environment.h>
+
+
+BOOST_AUTO_TEST_SUITE( EnvironmentParsingTest )
+BOOST_AUTO_TEST_CASE( ParseValidFcgiEnvironment )
+{
+     const char* params[] = {
+          "HTTP_REQUEST_VERB=GET",
+          "HTTP_VERSION=1.0",
+          "HTTP_CONTENT_TYPE=application/json",
+          "HTTP_CONTENT_LENGTH=123456",
+          "REQUEST_URI=/fcgi/service/name",
+          "REQUEST_METHOD=method",
+     };
+
+     using alexen::server::fcgi::Environment;
+
+     Environment env{ params };
+     BOOST_CHECK_EQUAL( env.get( "HTTP_REQUEST_VERB" ), "GET" );
+}
+BOOST_AUTO_TEST_SUITE_END()
