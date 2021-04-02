2a4e81fe0ec6bc59e8911bd6858b13d2b1726cbd
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-04-02 22:55:01 +0300

    server/test: Добавлен файл для юнит-тестов компонент сервера

---
+++ server/test/environment_test.cpp
@@ -0,0 +1,13 @@
+/// @file
+/// @brief
+/// @copyright Copyright (c) InfoTeCS. All Rights Reserved.
+
+#include "environment.h"
+
+namespace alexen {
+namespace server {
+namespace fcgi {
+
+} // namespace fcgi
+} // namespace server
+} // namespace alexen
