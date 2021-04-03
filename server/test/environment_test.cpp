80688e9b1dc8321aaa333b31327ecdffa3b63058
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-04-03 14:48:43 +0300

    server/test: Добавлен тест метода Environment::has()

--- server/test/environment_test.cpp
+++ server/test/environment_test.cpp
@@ -44,4 +44,23 @@ BOOST_AUTO_TEST_CASE( ParseFcgiEnvironmentWithEmptyValues )
      BOOST_CHECK_EQUAL( env.get( "KEY_HAS_EQUAL_SIGN" ), "" );
      BOOST_CHECK_EQUAL( env.get( "KEY_HAS_NO_EQUAL_SIGN" ), "" );
 }
+BOOST_AUTO_TEST_CASE( CheckEnvironmentKeyPresence )
+{
+     const char* const params[] = {
+          "HTTP_REQUEST_VERB=GET",
+          "HTTP_VERSION=1.0",
+          "HTTP_CONTENT_TYPE=application/json",
+          nullptr
+     };
+
+     using alexen::server::fcgi::Environment;
+
+     Environment env{ params };
+     BOOST_CHECK( env.has( "HTTP_REQUEST_VERB" ) );
+     BOOST_CHECK( env.has( "HTTP_VERSION" ) );
+     BOOST_CHECK( env.has( "HTTP_CONTENT_TYPE" ) );
+
+     BOOST_CHECK( not env.has( "http_request_verb" ) );
+     BOOST_CHECK( not env.has( "NO_SUCH_KEY" ) );
+}
 BOOST_AUTO_TEST_SUITE_END()
