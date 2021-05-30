8d2b7d081d8ae4920209ac4605fc4634f17cf98c
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-05-30 22:16:22 +0300

    tools: Добавлен пустой каркас для утилит

---
+++ tools/url_escape.cpp
@@ -0,0 +1,13 @@
+/// @file
+/// @brief
+/// @copyright Copyright (c) InfoTeCS. All Rights Reserved.
+
+#include <tools/url_escape.h>
+
+namespace alexen {
+namespace server {
+namespace tools {
+
+} // namespace tools
+} // namespace server
+} // namespace alexen
---
+++ tools/url_escape.h
@@ -0,0 +1,14 @@
+/// @file
+/// @brief
+/// @copyright Copyright (c) InfoTeCS. All Rights Reserved.
+
+#pragma once
+
+namespace alexen {
+namespace server {
+namespace tools {
+
+
+} // namespace tools
+} // namespace server
+} // namespace alexen
